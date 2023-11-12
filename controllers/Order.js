333b51eaf1d2f211e5cfc5bb8969e87b2a0aa7db
MunNX <thaiboomnguyen@gmail.com>
2023-11-12 11:45:14 +0700

    deploy be v11

--- controllers/Order.js
+++ controllers/Order.js
@@ -37,77 +37,77 @@ export const orderForm = async (req, res, next) => {
     }
 };
 
-export const exportOrderToExcelById = async (req, res) => {
-    const orderId = req.query.orderId;
+// export const exportOrderToExcelById = async (req, res) => {
+//     const orderId = req.query.orderId;
 
-    try {
-        const order = await orderSchema.findById(orderId);
+//     try {
+//         const order = await orderSchema.findById(orderId);
 
-        if (!order) {
-            return res.status(NOT_FOUND).json({ error: "Order not found" });
-        }
+//         if (!order) {
+//             return res.status(NOT_FOUND).json({ error: "Order not found" });
+//         }
 
-        const fileName = `order_${orderId}.xlsx`;
+//         const fileName = `order_${orderId}.xlsx`;
 
-        const workbook = new ExcelJS.Workbook();
-        const worksheet = workbook.addWorksheet('Order');
+//         const workbook = new ExcelJS.Workbook();
+//         const worksheet = workbook.addWorksheet('Order');
 
-        // Define the columns for the Excel sheet
-        const columns = [
-            { header: 'Pick Up Place', key: 'pick_up_place', width: 15 },
-            { header: 'Pick Up Date', key: 'pick_up_date', width: 15 },
-            { header: 'Products', key: 'products', width: 50 },
-            { header: 'First Name', key: 'first_name', width: 15 },
-            { header: 'Last Name', key: 'last_name', width: 15 },
-            { header: 'Email', key: 'email', width: 20 },
-            { header: 'Phone Number', key: 'phone_number', width: 15 },
-            { header: 'Zip Code', key: 'zip_code', width: 15 },
-            { header: 'City', key: 'city', width: 15 },
-            { header: 'Street', key: 'street', width: 15 },
-        ];
+//         // Define the columns for the Excel sheet
+//         const columns = [
+//             { header: 'Pick Up Place', key: 'pick_up_place', width: 15 },
+//             { header: 'Pick Up Date', key: 'pick_up_date', width: 15 },
+//             { header: 'Products', key: 'products', width: 50 },
+//             { header: 'First Name', key: 'first_name', width: 15 },
+//             { header: 'Last Name', key: 'last_name', width: 15 },
+//             { header: 'Email', key: 'email', width: 20 },
+//             { header: 'Phone Number', key: 'phone_number', width: 15 },
+//             { header: 'Zip Code', key: 'zip_code', width: 15 },
+//             { header: 'City', key: 'city', width: 15 },
+//             { header: 'Street', key: 'street', width: 15 },
+//         ];
 
-        // Set columns for the Excel sheet
-        worksheet.columns = columns;
+//         // Set columns for the Excel sheet
+//         worksheet.columns = columns;
 
-        const productsString = order.products.map(product => `${product.product_name}: ${product.product_quantity}`).join(', ');
+//         const productsString = order.products.map(product => `${product.product_name}: ${product.product_quantity}`).join(', ');
 
-        // Populate the worksheet with data
-        const rowData = {
-            pick_up_place: order.pick_up_place,
-            pick_up_date: order.pick_up_date,
-            products: productsString,
-            first_name: order.first_name,
-            last_name: order.last_name,
-            email: order.email,
-            phone_number: order.phone_number,
-            zip_code: order.zip_code,
-            city: order.city,
-            street: order.street,
-        };
-        worksheet.addRow(rowData);
+//         // Populate the worksheet with data
+//         const rowData = {
+//             pick_up_place: order.pick_up_place,
+//             pick_up_date: order.pick_up_date,
+//             products: productsString,
+//             first_name: order.first_name,
+//             last_name: order.last_name,
+//             email: order.email,
+//             phone_number: order.phone_number,
+//             zip_code: order.zip_code,
+//             city: order.city,
+//             street: order.street,
+//         };
+//         worksheet.addRow(rowData);
 
 
-        // Generate the Excel file in memory
-        const buffer = await workbook.xlsx.writeBuffer();
+//         // Generate the Excel file in memory
+//         const buffer = await workbook.xlsx.writeBuffer();
 
-        // Save the buffer to the file path
-        try {
-            fs.writeFileSync(fileName, buffer);
-            console.log(`Excel file saved to ${fileName}`);
-        } catch (error) {
-            console.error('Error saving the Excel file:', error);
-        }
+//         // Save the buffer to the file path
+//         try {
+//             fs.writeFileSync(fileName, buffer);
+//             console.log(`Excel file saved to ${fileName}`);
+//         } catch (error) {
+//             console.error('Error saving the Excel file:', error);
+//         }
 
-        // Set content type and attachment header with the generated file name
-        res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
-        // Send the buffer as the response
-        res.send(buffer);
-    } catch (error) {
-        console.error('Error exporting Excel:', error);
-        return res.status(SYSTEM_ERROR).json({ error: 'Internal server error' });
-    }
-};
+//         // Set content type and attachment header with the generated file name
+//         res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+//         res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
+//         // Send the buffer as the response
+//         res.send(buffer);
+//     } catch (error) {
+//         console.error('Error exporting Excel:', error);
+//         return res.status(SYSTEM_ERROR).json({ error: 'Internal server error' });
+//     }
+// };
 
 export const exportOrdersToExcel = async (req, res) => {
     try {
--- routes/order.js
+++ routes/order.js
@@ -1,13 +1,10 @@
 import express from 'express';
-import {
-    exportOrdersToExcel, exportOrderToExcelById,
-    getAllOrder, orderForm
-} from '../controllers/orderController.js';
+import { exportOrdersToExcel, getAllOrder, orderForm } from '../controllers/Order.js';
 
 const router = express.Router();
 
 router.post('/orderForm', orderForm);
-router.post('/export-xlsx-byId', exportOrderToExcelById);
+// router.post('/export-xlsx-byId', exportOrderToExcelById);
 router.post('/export-xlsx', exportOrdersToExcel);
 router.get('/get', getAllOrder);
 
