8ff289a5f1ec49e3b4859459c709aadd6c8a2f08
MunNX <thaiboomnguyen@gmail.com>
2023-11-13 11:12:20 +0700

    deploy be v35

--- routes/order.js
+++ routes/order.js
@@ -7,5 +7,8 @@ router.post('/orderForm', orderForm);
 // router.post('/export-xlsx-byId', exportOrderToExcelById);
 router.post('/export-xlsx', exportOrdersToExcel);
 router.get('/get', getAllOrder);
+router.get('/home', (req, res) => {
+        res.status(200).json('Welcome, your app is working well');
+      })
 
 export default router;
