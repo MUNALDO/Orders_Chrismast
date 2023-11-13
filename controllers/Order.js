868aeac41e55d2f90ebafdace19edc1b24740bb9
MunNX <thaiboomnguyen@gmail.com>
2023-11-13 21:58:36 +0700

    deploy be v62

--- controllers/Order.js
+++ controllers/Order.js
@@ -10,37 +10,37 @@ export const orderForm = async (req, res, next) => {
     const { pick_up_place, pick_up_date, products, first_name, last_name,
         email, phone_number, zip_code, city, street } = req.body;
 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: false,
-        auth: {
-            user: process.env.MAIL_ADDRESS,
-            pass: process.env.MAIL_PASSWORD,
-        },
-    });
-
-    const sendEmail = (to, cc, subject, text) => {
-        return new Promise((resolve, reject) => {
-            transporter.sendMail({
-                from: process.env.MAIL_ADDRESS,
-                to,
-                cc,
-                subject,
-                text,
-            }, (error, info) => {
-                if (error) {
-                    console.error('Error sending email:', error);
-                    reject(error);
-                } else {
-                    console.log('Email sent:', info.messageId);
-                    resolve(info);
-                }
-            });
-        });
-    };
-
     try {
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.MAIL_ADDRESS,
+                pass: process.env.MAIL_PASSWORD,
+            },
+        });
+    
+        const sendEmail = (to, cc, subject, text) => {
+            return new Promise((resolve, reject) => {
+                transporter.sendMail({
+                    from: process.env.MAIL_ADDRESS,
+                    to,
+                    cc,
+                    subject,
+                    text,
+                }, (error, info) => {
+                    if (error) {
+                        console.error('Error sending email:', error);
+                        reject(error);
+                    } else {
+                        console.log('Email sent:', info.messageId);
+                        resolve(info);
+                    }
+                });
+            });
+        };
+    
         const newOrder = new order({
             pick_up_place: pick_up_place,
             pick_up_date: pick_up_date,
