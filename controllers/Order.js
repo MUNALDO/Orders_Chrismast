f4a66d115a4611f321291b66d5e149b1ce0c5b99
MunNX <thaiboomnguyen@gmail.com>
2023-11-13 11:55:56 +0700

    deploy be v38

--- controllers/Order.js
+++ controllers/Order.js
@@ -176,7 +176,7 @@ export const getAllOrder = async (req, res, next) => {
     try {
         const orders = await order.find({});
         console.log(orders);
-        res.json(orders);
+        return res.json(orders);
     } catch (err) {
         next(err);
     }
