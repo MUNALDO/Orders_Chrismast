5f3c152a32c9f020de310fe495be9a38d9dd1272
MunNX <thaiboomnguyen@gmail.com>
2023-11-13 13:56:36 +0700

    deploy be v44

--- routes/order.js
+++ routes/order.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { exportOrdersToExcel, getAllOrder, orderForm } from '../controllers/Order.js';
-import order from '../models/order.js';
 
 const router = express.Router();
 
@@ -11,15 +10,5 @@ router.get('/get', getAllOrder);
 router.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
 })
-router.get('/get-orders', async (req, res, next) => {
-  try {
-    const orders = await order.find();
-    console.log(orders);
-    res.status(200).json(orders);
-  } catch (err) {
-    next(err);
-  }
-});
-
 
 export default router;
