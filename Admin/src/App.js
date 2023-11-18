f17a98fcd0742748e0772405139ba4c6ae760100
MunNX <thaiboomnguyen@gmail.com>
2023-11-19 03:04:05 +0700

    add admin + be

---
+++ Admin/src/App.js
@@ -0,0 +1,39 @@
+import List from "./pages/list/List";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { doctorInputs, userInputs } from "./formSource";
+import "./style/dark.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
+// import { AuthContext } from "./context/AuthContext";
+import { ordersColumns } from "./datatablesource";
+import Export from "./pages/export_xlsx/Export";
+
+
+function App() {
+  const { darkMode } = useContext(DarkModeContext);
+
+  return (
+    <div className={darkMode ? "app dark" : "app"}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route
+              index
+              element={
+                <List columns={ordersColumns} />
+              }
+            />
+            <Route
+              path="export-xlsx"
+              element={
+                <Export title="Export Excel" />
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ Admin/src/pages/export_xlsx/Export.jsx
@@ -0,0 +1,56 @@
+import "./new.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import { useNavigate } from "react-router-dom";
+
+const Export = ({ title }) => {
+  const navigate = useNavigate();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/order/export-xlsx", { method: "POST" });
+
+      if (response.ok) {
+        // Trigger download by creating a Blob and using the URL.createObjectURL
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "orders.xlsx";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+
+        console.log("Export successful");
+      } else {
+        console.error(`Export failed with status: ${response.status}`);
+      }
+    } catch (err) {
+      console.error("Error exporting:", err);
+    }
+  };
+
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>{title}</h1>
+        </div>
+        <div className="bottom">
+          <div className="right">
+            <form>
+              <button onClick={handleClick}>Export</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Export;
